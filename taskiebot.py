f6d9eee4ee16985e1c7d8521b1de18821ed6a710
elseagle <dowolebolu@gmail.com>
2018-06-01 01:23:36 +0100

    adding changing priorities in pingserver

--- taskiebot.py
+++ taskiebot.py
@@ -134,7 +134,7 @@ class myThread(threading.Thread):
 
 def parse_datetime_from(date, times):
 	t1 = str(date) + str(times)
-	print(t1)S
+	print(t1)
 	t1 = time.strptime(t1, '%Y-%m-%d%H:%M:%S')
 	time_now = time.time()
 	time_set = time.mktime(t1)
